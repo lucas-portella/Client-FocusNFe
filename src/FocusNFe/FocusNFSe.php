4def1e2899fd704e96cf9c414a2d6606c0ed420a
Lucas Portella <lucasportella@hotmail.com>
2026-01-19 15:47:26 -0300

    Emissão de NFSe

--- src/FocusNFe/FocusNFSe.php
+++ src/FocusNFe/FocusNFSe.php
@@ -15,25 +15,84 @@ class FocusNFSe
 
     private function geraPayloadEmissaoNFSe (array $dadosServico): array
     {
-        return [];
+        $payload = [
+            "cnpj_prestador" => (string) $dadosServico['cnpj_prestador'] ?? null,
+            "cpf_prestador" => (string) $dadosServico['cpf_prestador'],
+            "data_emissao" => (string) $dadosServico['data_emissao'] ?? null,
+            "serie_dps" => (int) $dadosServico['serie_dps'] ?? null,
+            "numero_dps" => (int) $dadosServico['numero_dps'] ?? null,
+            "data_competencia" => (string) $dadosServico['data_competencia'] ?? null,
+            "emitente_dps" => (int) $dadosServico['emitente_dps'] ?? null,
+            "codigo_municipio_emissora" => (int) $dadosServico['codigo_municipio_emissora'] ?? null,
+            "codigo_opcao_simples_nacional" => (int) $dadosServico['codigo_opcao_simples_nacional'] ?? null,
+            "regime_especial_tributacao" => (int) $dadosServico['regime_especial_tributacao'] ?? null,
+            "cpf_tomador" => (string) $dadosServico['cpf_tomador'] ?? null,
+            "codigo_municipio_prestacao" => (int) $dadosServico['codigo_municipio_prestacao'] ?? null,
+            "codigo_tributacao_nacional_iss" => (int) $dadosServico['codigo_tributacao_nacional_iss'] ?? null,
+            "descricao_servico" => (string) $dadosServico['descricao_servico'] ?? null,
+            "codigo_nbs" => (int) $dadosServico['codigo_nbs'] ?? null,
+            "valor_servico" => (float) $dadosServico['valor_servico'] ?? null,
+            "tributacao_iss" => (int) $dadosServico['tributacao_iss'] ?? null,
+            "finalidade_emissao" => (int) $dadosServico['finalidade_emissao'] ?? null,
+            "consumidor_final" => (int) $dadosServico['consumidor_final'] ?? null,
+            "indicador_destinatario" => (int) $dadosServico['indicador_destinatario'] ?? null,
+            "ibs_cbs_situacao_tributaria" => (string) $dadosServico['ibs_cbs_situacao_tributaria'] ?? null,
+            "ibs_cbs_classificacao_tributaria" => (string) $dadosServico['ibs_cbs_classificacao_tributaria'] ?? null
+        ];
+
+        return $payload;
     }
 
-    private function geraPayloadCancelamentoNFSe (array $dadosServico): array
+    public function emitirNFSe (array $dadosServico, string $referenciaNFSe): array
     {
-        return [];
+        $uri = "/v2/nfsen?ref={$referenciaNFSe}";
+        $payload = $this->geraPayloadEmissaoNFSe($dadosServico);
+
+        try {
+            return $this->client->request('POST', $uri, $payload);
+        } catch (Exception $e) {
+            return [
+                'status' => 500,
+                'data' => [
+                    'mensagem' => 'Erro ao emitir NFSe',
+                    'detalhe' => $e->getMessage()
+                ]
+            ];
+        }
     }
 
-    public function emitirNFSe (): array
+    public function consultarNFSe (string $referenciaNFSe): array
     {
-        return [];
+        $uri = "/v2/nfsen/{$referenciaNFSe}";
+
+        try {
+            return $this->client->request('GET', $uri);
+        } catch (Exception $e) {
+            return [
+                'status' => 500,
+                'data' => [
+                    'mensagem' => "Erro ao consultar NFSe {$referenciaNFSe}",
+                    'detalhe' => $e->getMessage()
+                ]
+            ];
+        }
     }
 
-    public function consultarNFSe (string $id): array
+    public function cancelarNFSe (string $referenciaNFSe, string $justificativa): array 
     {
-        return [];
-    }
-
-    public function cancelarNFSe (string $id): array {
-        return [];
+        $uri = "/v2/nfsen/{$referenciaNFSe}";
+        $payload = ['justificativa' => $justificativa];
+    
+        try {
+            return $this->client->request ('DELETE', $uri, $payload);
+        } catch (Exception $e) {
+            return [
+                'status' => 500,
+                'data' => [
+                    'mensagem' => "Erro ao cancelar NFSe {$referenciaNFSe}",
+                    'detalhe' => $e->getMessage()
+                ]
+            ];
+        }
     }
 }
